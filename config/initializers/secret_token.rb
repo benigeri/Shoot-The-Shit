0e249f0815ed672cbcd5e618ceeb5a1d99aebae3
Paul Benigeri <me@benigeri.com>
2012-10-22 14:35:44 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShootTheShit::Application.config.secret_token = '970897e2656af91dcedbb337ebb5c835f57e3b63a5a2c1b6e57e7a66e98c55b408a1221a2bc8dbfa3cab5b5c052fd35e0b26e01a712e19b6da20a36ffb028a06'
