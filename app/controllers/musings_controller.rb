a7fd08cf45c3d9ea1b26a005249d64ed73ee71c7
Paul Benigeri <me@benigeri.com>
2012-10-29 23:55:17 -0700

    use thin web server, change musing from to have large text box for content, create musing show page, add redirect from new musing to musing show page

--- app/controllers/musings_controller.rb
+++ app/controllers/musings_controller.rb
@@ -1,5 +1,6 @@
 class MusingsController < ApplicationController
   def show
+    @musing = Musing.find(params[:id])
   end
 
   def new
@@ -7,6 +8,17 @@ class MusingsController < ApplicationController
   end
 
   def create
+    @musing = Musing.new(params[:musing])
+
+    respond_to do |format|
+      if @musing.save
+        format.html { redirect_to @musing, :notice => 'Musing was successfully created.' }
+        format.json { render :json => @musing }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @product.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
 end
