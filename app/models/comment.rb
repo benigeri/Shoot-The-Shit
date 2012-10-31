ad8e707bcfae40641d56b146aa075a418fcb4fce
Paul Benigeri <me@benigeri.com>
2012-10-31 01:30:20 -0700

    validate comment had content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
    attr_accessible :content, :user_id, :musing_id
   # attr_accessible :content
+   validates :content, :presence => true
    belongs_to :musing
    belongs_to :user
 end
