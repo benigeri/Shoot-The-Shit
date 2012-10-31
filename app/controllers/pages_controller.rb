1882c3c0536bc54f05e60a2cb73fe2376b3a868c
Paul Benigeri <me@benigeri.com>
2012-10-31 01:20:22 -0700

    updated todo, off to bed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   before_filter :set_timezone
-  def set_timezone
-    Time.zone =  'Pacific Time (US & Canada)'
-  end
+
 
   def musings
     @musings = Musing.all
