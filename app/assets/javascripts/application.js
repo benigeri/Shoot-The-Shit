b877caa6b31eed89a12b02e35647696c8a75f80a
Paul Benigeri <me@benigeri.com>
2012-11-03 01:00:36 -0700

    fix a lot of location bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+//DEFINITIONS
+  var PASS_LOCATION_TO_CONTROLLER = 0;
+  var SET_LOCATION_DEF = 1;
+  var callbackSwitch;
+
   function toggleClass(elem) {
     $("a.pillbox").removeClass("selected");
     $(elem).addClass("selected");
@@ -21,3 +27,56 @@
     evt.preventDefault();
     window.history.back();
   }
+
+  var locationObject = {
+    name : "",
+    longitude : "",
+    latitude : ""
+  };
+
+  function initLocation() {
+    if (navigator.geolocation && typeof (navigator.geolocation.getCurrentPosition) == "function") {
+      navigator.geolocation.getCurrentPosition(geoCodeCallback);
+    }
+  }
+
+  function geoCodeCallback(position) {
+    locationObject.longitude = position.coords.longitude;
+    locationObject.latitude = position.coords.latitude;
+    var latLng = new google.maps.LatLng(locationObject.latitude, locationObject.longitude);
+    var coder = new google.maps.Geocoder();
+    coder.geocode({ 'latLng': latLng }, showLocaleCallback);
+  }
+
+  function showLocaleCallback(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var res = results[0];
+      for (var i = 0; i < res.address_components.length; i++) {
+        var type = res.address_components[i].types[0];
+        if (type == "subpremise" || type == "premise" || type == "neighborhood" || type == "sublocality" || type == "locality") {
+          locationObject.name = res.address_components[i].short_name;
+          finalCallback(locationObject);
+          return;
+        }
+      }
+    }
+  }
+
+  function finalCallback(object){
+    switch(callbackSwitch){
+      case 0:
+        passLocationToController(object);
+        break;
+      case 1:
+        setLocation(object);
+        break;
+}
+  }
+
+
+  function setLocation(object) {
+    var form = document.getElementById("new_musing");
+    form.musing_latitude.value = object.latitude;
+    form.musing_longitude.value = object.longitude;
+    form.musing_city.value = object.name;
+}
--- app/assets/javascripts/musings.js
+++ app/assets/javascripts/musings.js
@@ -2,11 +2,11 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 function setLocation() {
-  if(google.loader.ClientLocation) {
+  if(initLocation()){
     var form = document.getElementById("new_musing");
-    form.musing_latitude.value = google.loader.ClientLocation.latitude;
-    form.musing_longitude.value = google.loader.ClientLocation.longitude;
-    form.musing_city.value = google.loader.ClientLocation.address.city;
+    form.musing_latitude.value = locationObject.latitude;
+    form.musing_longitude.value = locationObject.longitude;
+    form.musing_city.value = locationObject.name;
   } else {
     alert("Unfortunately we were not able to get your location.")
   }
