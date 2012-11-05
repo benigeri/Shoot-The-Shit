046844361accb19f85048c2e270eeeb4424b388c
Paul Benigeri <me@benigeri.com>
2012-11-05 15:39:41 -0800

    fix voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -13,21 +13,33 @@ class VotesController < ApplicationController
 
   ##BUG: when revote, need to increment by 2 not 1
   def create
-    @vote = current_user.votes.where(:musing_id => params[:vote][:musing_id]).first ||  current_user.votes.create(params[:vote])
+    @oldVote = current_user.votes.where(:musing_id => params[:vote][:musing_id]).first
+    @vote = @oldVote ||  current_user.votes.create(params[:vote])
     @vote.update_attributes(:up => params[:vote][:up])
 
     @musing = Musing.where(:id => @vote.musing_id).first
-
-    if @vote.up
-     if @musing.save
+    if !(@oldVote)
+      if (@vote.up)
         @musing.votecount += 1
-     end
-    else
-      if @musing.save
+      else
         @musing.votecount -= 1
       end
+      @musing.save
+    else
+      if @vote.up
+        if (@oldVote.up)
+          @musing.votecount += 2
+        end
+      else
+        if !(@oldVote.up)
+          @musing.votecount -= 2
+        end
+      end
+      @musing.save
     end
     redirect_to :back
   end
 
 end
+
+
