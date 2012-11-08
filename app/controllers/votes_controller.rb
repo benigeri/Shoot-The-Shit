ae8f6323b26e0c7616d9293d2e8399b58173a71d
Paul Benigeri <me@benigeri.com>
2012-11-08 04:27:45 -0800

    voting actually works as expected now

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,32 +14,38 @@ class VotesController < ApplicationController
   ##BUG: when revote, need to increment by 2 not 1
   def create
     @oldVote = current_user.votes.where(:musing_id => params[:vote][:musing_id]).first
-    @vote = @oldVote ||  current_user.votes.create(params[:vote])
-    @vote.update_attributes(:up => params[:vote][:up])
+    @musing = Musing.where(:id => params[:vote][:musing_id]).first
 
-    @musing = Musing.where(:id => @vote.musing_id).first
-    if !(@oldVote)
-      if (@vote.up)
+
+    if @oldVote
+      @oldVoteUp = @oldVote.up
+      @newVoteUp = params[:vote][:up]
+      if (@oldVoteUp == true)
+        if (@newVoteUp != false)
+          @musing.votecount -=2
+          @oldVote.update_attributes(:up => false)
+          @oldVote.save
+          @musing.save
+        end
+      else
+        if (@newVoteUp != true)
+          @musing.votecount +=2
+          @oldVote.update_attributes(:up => true)
+          @oldVote.save;
+          @musing.save
+        end
+      end
+    else
+      @vote = current_user.votes.create(params[:vote])
+      if @vote.up
         @musing.votecount += 1
       else
         @musing.votecount -= 1
       end
-      @musing.save
-    else
-      if @vote.up
-        if (@oldVote.up)
-          @musing.votecount += 2
-        end
-      else
-        if !(@oldVote.up)
-          @musing.votecount -= 2
-        end
-      end
-      @musing.save
     end
+    @musing.save
     redirect_to :back
   end
-
 end
 
 
