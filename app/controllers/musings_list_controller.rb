759a450a33e127be9aafd7e515cd3ec4a54867e4
Paul Benigeri <me@benigeri.com>
2012-11-07 21:22:50 -0800

    remove auth for blitz test

--- app/controllers/musings_list_controller.rb
+++ app/controllers/musings_list_controller.rb
@@ -1,5 +1,5 @@
 class MusingsListController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
   before_filter :set_timezone
   before_filter :set_globals
   def set_globals
